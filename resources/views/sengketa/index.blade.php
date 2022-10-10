5517a61990576ac0249260cb250e50699cbd7488
isa-ardi <isaardi21@gmail.com>
2022-10-10 14:18:09 +0700

    komit

--- resources/views/sengketa/index.blade.php
+++ resources/views/sengketa/index.blade.php
@@ -686,61 +686,7 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand fw-bold fs-4" href="{{url('/')}}">
-                <img src="{{asset('/uploads/sengketa-logo_pas.png')}}" style="width:30px"
-                    class="img-fluid me-2">SENGKETA TANAH</a>
-            <style>
-                .form-check-input:checked {
-                    background-color: red border-color: pink;
-                }
-
-            </style>
-            <div class="form-check form-switch">
-                <input class="form-check-input dark-mode" type="checkbox" role="switch" id="dark-mode">
-            </div>
-            <button class="navbar-toggler pe-3 ps-3 pt-2 pb-2" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <i class="fa-solid fa-bars"></i>
-            </button>
-
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav ms-auto">
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{url('/')}}">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Produk</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Panduan</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Jaminan</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Hubungi Kami</a>
-                    </li>
-                    <li class="nav-item me-2">
-                        <a class="nav-link" href="{{url('/')}}/tentang">Tentang Kami</a>
-                    </li>
-
-                    <div class="vr vr-none me-2"></div>
-                    <hr class="hr-none">
-
-                    <li class="nav-item me-1 ms-1">
-                        <a class="nav-link btn btn-outline-brown rounded-3 login-mb" href="{{url('/')}}/login">Login</a>
-                    </li>
-                    <li class="nav-item me-1 ms-1">
-                        <a class="nav-link btn btn-brown text-white rounded-3" href="{{url('/')}}/register">Register</a>
-                    </li>
-                </ul>
-
-            </div>
-        </div>
-    </nav>
+    @include('layouts.template-public.header')
 
     <div id="carouselMobile" class="carousel slide mobile" data-bs-ride="carousel">
         <div class="carousel-inner">
